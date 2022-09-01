3d0c8d755ea191a37fd4157ede0fb5cab047a8f0
limitless789 <dongwook.dc@gmail.com>
2022-09-02 02:56:05 +0900

    info_basic
    added basic info with 5 markers

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 var map;
 var img_park;
-
+var marker;
 function initMap() {
   let database = JSON.parse(JSON.stringify(data));
 
@@ -14,7 +14,7 @@ function initMap() {
   };
   img_center = {
     num: 0,
-    url: "./img/parks.png",
+    url: "./img/center.png",
     scaledSize: new google.maps.Size(40, 40),
   };
   var img_arr = new Array(img_park, img_center);
@@ -40,24 +40,49 @@ function initMap() {
   });
 
   //add markers from database of data.json
-  for (var i = 0; i < database.length; i++) {
-    new google.maps.Marker({
+  /*for (var i = 0; i < database.length; i++) {
+    marker = new google.maps.Marker({
       position: new google.maps.LatLng( //position of marker
         database[i].position[0],
         database[i].position[1]
       ),
       map: map,
       icon: img_arr[database[i].icon], //image format of marker
-      label: {
+      /*label: {
         //label of marker
-        text: /*img_arr[database[i].icon].nm +*/ String(
+        text: /*img_arr[database[i].icon].nm + String(
           img_arr[database[i].icon].num++
         ),
         color: "black",
         fontSize: "17px",
         fontWeight: "bold",
       },
-      title: database[i].title,
+    });
+    var contentString = database[i].title;
+
+    var infowindow = new google.maps.InfoWindow({ content: contentString });
+
+    google.maps.event.addListener(marker, "click", function () {
+      infowindow.open(map, marker);
+    });
+  }*/
+  for (index in database) addMarker(database[index]);
+  var prev_info = false;
+  function addMarker(data) {
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(data.position[0], data.position[1]),
+      map: map,
+      icon: img_arr[data.icon],
+    });
+
+    var contentString = data.title;
+    var infowindow = new google.maps.InfoWindow({ content: contentString });
+    google.maps.event.addListener(marker, "click", function () {
+      if (prev_info) {
+        prev_info.close();
+      }
+      infowindow.open(map, marker);
+      prev_info = infowindow;
     });
   }
 }
