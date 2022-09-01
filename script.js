e71dcdb5221fe1ea68900aa36c1bbbebd9989ddc
limitless789 <dongwook.dc@gmail.com>
2022-09-01 15:11:44 +0900

    3data_json

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //script.js
 
 var map;
+var img_park;
 
 function initMap() {
+  let database = JSON.parse(JSON.stringify(data));
+
+  //array of image
+  img_park = {
+    num: 0,
+    url: "./img/parks.png",
+    scaledSize: new google.maps.Size(40, 40),
+  };
+  img_center = {
+    num: 0,
+    url: "./img/parks.png",
+    scaledSize: new google.maps.Size(40, 40),
+  };
+  var img_arr = new Array(img_park, img_center);
+
+  //array of area
   var bucheon = { lat: 37.5036901, lng: 126.786974 };
+  //delete default markers of google map
   var myStyles = [
     {
       featureType: "poi",
@@ -11,10 +29,8 @@ function initMap() {
       stylers: [{ visibility: "off" }],
     },
   ];
-  var imaging = {
-    url: "./img/parks.png",
-    scaledSize: new google.maps.Size(30, 30),
-  };
+
+  //map
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 13,
     center: bucheon,
@@ -23,15 +39,25 @@ function initMap() {
     streetViewControl: false,
   });
 
-  new google.maps.Marker({
-    position: { lat: 37.5006223, lng: 126.7656312 },
-    map: map,
-    icon: imaging,
-    title: "중앙공원",
-  });
-  new google.maps.Marker({
-    position: { lat: 37.4947512, lng: 126.75401356 },
-    map: map,
-    label: "복사골 문화센터",
-  });
+  //add markers from database of data.json
+  for (var i = 0; i < database.length; i++) {
+    new google.maps.Marker({
+      position: new google.maps.LatLng( //position of marker
+        database[i].position[0],
+        database[i].position[1]
+      ),
+      map: map,
+      icon: img_arr[database[i].icon], //image format of marker
+      label: {
+        //label of marker
+        text: /*img_arr[database[i].icon].nm +*/ String(
+          img_arr[database[i].icon].num++
+        ),
+        color: "black",
+        fontSize: "17px",
+        fontWeight: "bold",
+      },
+      title: database[i].title,
+    });
+  }
 }
