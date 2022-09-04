05b43a4963868e33daf1a1a9185016b774a87202
limitless789 <dongwook.dc@gmail.com>
2022-09-04 13:50:47 +0900

    optimize

--- script.js
+++ script.js
@@ -53,35 +53,12 @@ var marker;
 function initMap() {
   let database = JSON.parse(JSON.stringify(data));
 
-  //array of image
-  img_park = {
-    num: 0,
-    url: "./img/parks.png",
-    scaledSize: new google.maps.Size(40, 40),
-  };
-  img_center = {
-    num: 0,
-    url: "./img/center.png",
-    scaledSize: new google.maps.Size(40, 40),
-  };
-  var img_arr = new Array(img_park, img_center);
-
   //array of area
   var bucheon = { lat: 37.5036901, lng: 126.786974 };
-  //delete default markers of google map
-  var myStyles = [
-    {
-      featureType: "poi",
-      elementType: "labels",
-      stylers: [{ visibility: "off" }],
-    },
-  ];
-
   //map
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 13,
     center: bucheon,
-    styles: myStyles,
     mapTypeControl: false,
     streetViewControl: false,
     styles: mapStyle,
@@ -107,6 +84,7 @@ function initMap() {
           </div>`;
     var infowindow = new google.maps.InfoWindow({ content: contentString });
     google.maps.event.addListener(marker, "click", function () {
+      map.setCenter(this.getPosition());
       if (prev_info) {
         prev_info.close();
       }
