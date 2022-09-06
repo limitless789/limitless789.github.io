66be1e92f9c6e41507ab5b2226c996a1330cada7
limitless789 <dongwook.dc@gmail.com>
2022-09-06 19:28:06 +0900

    slider1
    slider1

--- script.js
+++ script.js
@@ -48,11 +48,12 @@ const mapStyle = [
 ];
 
 var map;
-var img_park;
-var marker;
+let hash_map = new Map();
+var markers = new Map();
+let hash_num = new Map();
 function initMap() {
   let database = JSON.parse(JSON.stringify(data));
-
+  var marker;
   //array of area
   var bucheon = { lat: 37.5036901, lng: 126.786974 };
   //map
@@ -69,27 +70,98 @@ function initMap() {
   for (index in database) addMarker(database[index]);
   var prev_info = false;
   function addMarker(data) {
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(data.position[0], data.position[1]),
-      map: map,
-      icon: {
-        url: `./img/${data.type}.png`,
-        scaledSize: new google.maps.Size(40, 40),
-      },
-    });
-
-    var contentString = `<div style="text-align:center">
-          <h2 style="font-size:25px; padding: 5px 0px 10px 0px;">${data.title}</h2>
-          <p><img src="./img/view/${data.pic}" width="480" height="270" style="margin: 10px 10px 0px 10px"></p>
+    if (!hash_map.has(data.place)) {
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.position[0], data.position[1]),
+        map: map,
+        icon: {
+          url: `./img/${data.type}.png`,
+          scaledSize: new google.maps.Size(40, 40),
+        },
+        id: data.place,
+      });
+      var contentString = `<div style="text-align:center">
+            <h2 style="font-size:25px; padding: 5px 0px 10px 0px;">${data.title}</h2>
+            <div id="imgcenter">
+              <p><img src="./img/view/${data.pic}" width="480" height="270" style="margin: 10px 10px 0px 10px"></p>
+                <div style="position:absolute; bottom:125px; right:16px">
+                  <button type="button" onclick="right_click_action('${data.place}')" style="border: 0; outline:none; background-color:transparent; cursor:pointer">
+                  <img  src="./img/button.png" width="40" height="40"></button>
+                </div>
+                <div style="position:absolute; bottom:125px; right:456px">
+                  <button type="button" onclick="left_click_action('${data.place}')" style="border: 0; outline:none; background-color:transparent; cursor:pointer">
+                  <img  src="./img/button.png" width="40" height="40" style="transform: scaleX(-1)"></button>
+                </div>
+            </div>
           </div>`;
-    var infowindow = new google.maps.InfoWindow({ content: contentString });
-    google.maps.event.addListener(marker, "click", function () {
-      map.setCenter(this.getPosition());
-      if (prev_info) {
-        prev_info.close();
+      var infowindow = new google.maps.InfoWindow({ content: contentString });
+      google.maps.event.addListener(marker, "click", function () {
+        map.setCenter(this.getPosition());
+        if (prev_info) {
+          prev_info.close();
+        }
+        infowindow.open(map, marker);
+        prev_info = infowindow;
+      });
+      infos = new Array();
+      infos.push(infowindow);
+      markers.set(data.place, marker);
+      hash_map.set(data.place, infos);
+      hash_num.set(data.place, 0);
+    } else {
+      var contentString = `<div style="text-align:center">
+            <h2 style="font-size:25px; padding: 5px 0px 10px 0px;">${data.title}</h2>
+            <div id="imgcenter">
+              <p><img src="./img/view/${data.pic}" width="480" height="270" style="margin: 10px 10px 0px 10px"></p>
+                <div style="position:absolute; bottom:125px; right:26px">
+                  <button type="button" onclick="right_click_action('${data.place}')" style="border: 0; outline:none; background-color:transparent; cursor:pointer">
+                  <img  src="./img/button.png" width="40" height="40"></button>
+                </div>
+                <div style="position:absolute; bottom:125px; right:456px">
+                  <button type="button" onclick="left_click_action('${data.place}')" style="border: 0; outline:none; background-color:transparent; cursor:pointer">
+                  <img  src="./img/button.png" width="40" height="40" style="transform: scaleX(-1)"></button>
+                </div>
+            </div>
+          </div>`;
+      var infowindow = new google.maps.InfoWindow({ content: contentString });
+      var infos = hash_map.get(data.place);
+      infos.push(infowindow);
+      hash_map.set(data.place, infos);
+    }
+  }
+}
+
+function right_click_action(place) {
+  if (hash_map.has(place)) {
+    var infos = hash_map.get(place);
+    if (infos.length == 1) {
+      console.log("hello");
+      return;
+    } else {
+      var number = hash_num.get(place);
+      if (number + 1 < infos.length) {
+        var content = infos[number + 1].getContent();
+        infos[number + 1].setContent(infos[0].getContent());
+        infos[0].setContent(content);
+        console.log("hi");
+        hash_num.set(place, number + 1);
       }
-      infowindow.open(map, marker);
-      prev_info = infowindow;
-    });
+    }
+  }
+}
+function left_click_action(place) {
+  if (hash_map.has(place)) {
+    var infos = hash_map.get(place);
+    if (infos.length == 1) {
+      console.log("hello");
+      return;
+    } else {
+      var number = hash_num.get(place);
+      var content = infos[number].getContent();
+      infos[number].setContent(infos[0].getContent());
+      infos[0].setContent(content);
+      console.log("hi");
+      if (number - 1 >= 0) hash_num.set(place, number - 1);
+    }
   }
 }
